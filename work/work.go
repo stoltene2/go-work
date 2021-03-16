43d0f9a4cd6aa46ff9d576ac3109b6e98502e47c
Eric Stolten <stoltene2@gmail.com>
2021-03-15 21:25:11 -0400

    Add implementation for reference

--- work/work.go
+++ work/work.go
@@ -15,21 +15,38 @@ type Pool struct {
 
 func New(maxGoroutines int) *Pool {
 	// Initialize the Pool
+	pool := Pool{
+		work: make(chan Worker),
+	}
 
 	// Make maxGoroutines
+	pool.wg.Add(maxGoroutines)
+
 	// Register Goroutines with the waitgroup
+
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for t := range pool.work {
+				t.Task()
+			}
+
+			pool.wg.Done()
+		}()
+	}
+
 	// When each worker finishes because the worker channel is closed then update the waitgroup
-	return nil
+	return &pool
 }
 
 // Enqueue another task to be run
 // This call will block until a worker can pick up the Task
 func (p *Pool) Run(w Worker) {
-
+	p.work <- w
 }
 
 // Shutdown all the worker queue
 // Block until all Goroutines finish
 func (p *Pool) Shutdown() {
-
+	close(p.work)
+	p.wg.Wait()
 }
